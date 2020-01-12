803ce0c601e13216068262f9a62bcfc239b0689a
NiklasPor <niklaspor@gmail.com>
2020-01-12 18:56:02 +0100

    test abstractControlDirective css classes

---
+++ projects/ngrx-clean-forms/src/lib/directives/controls/abstract-control.directive.spec.ts
@@ -0,0 +1,139 @@
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { ReplaySubject } from 'rxjs';
+import { initFormControl } from '../../init';
+import { NgrxCleanFormsModule } from '../../ngrx-clean-forms.module';
+import { getFormControlSummary } from '../../selectors';
+import { FormControlSummary, FormControlUpdate } from '../../types';
+import { TextInputControlDirective } from './text-input-control.directive';
+
+@Component({
+    selector: 'ngrx-test-component',
+    template: `
+        <input
+            ngrxControl
+            type="text"
+            [controlSummary$]="summary$"
+            (controlUpdate)="update($event)"
+        />
+    `,
+})
+class TestComponent {
+    summary$: ReplaySubject<FormControlSummary<string>>;
+    update: () => {};
+}
+
+describe('TextInputControlDirective', () => {
+    let testComponent: ComponentFixture<TestComponent>;
+
+    let directiveDebug: DebugElement;
+    let directive: TextInputControlDirective;
+
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [NgrxCleanFormsModule],
+            declarations: [TestComponent],
+        });
+
+        testComponent = TestBed.createComponent(TestComponent);
+        testComponent.componentInstance.summary$ = new ReplaySubject(1);
+        testComponent.componentInstance.update = () => ({});
+
+        testComponent.detectChanges();
+
+        directiveDebug = testComponent.debugElement.query(By.directive(TextInputControlDirective));
+
+        directive = directiveDebug.injector.get(TextInputControlDirective);
+    });
+
+    it('{pristine: true} sets ng-pristine and unsets ng-dirty', () => {
+        testComponent.componentInstance.summary$.next({
+            ...getFormControlSummary(initFormControl([''])),
+            pristine: true,
+        });
+
+        // tslint:disable-next-line: no-string-literal
+        const isPristine = directive['ref'].nativeElement.classList.contains('ng-pristine');
+        // tslint:disable-next-line: no-string-literal
+        const isDirty = directive['ref'].nativeElement.classList.contains('ng-dirty');
+
+        expect(isPristine).toBe(true);
+        expect(isDirty).toBe(false);
+    });
+
+    it('{pristine: false} sets ng-dirty and unsets ng-pristine', () => {
+        testComponent.componentInstance.summary$.next({
+            ...getFormControlSummary(initFormControl([''])),
+            pristine: false,
+        });
+
+        // tslint:disable-next-line: no-string-literal
+        const isPristine = directive['ref'].nativeElement.classList.contains('ng-pristine');
+        // tslint:disable-next-line: no-string-literal
+        const isDirty = directive['ref'].nativeElement.classList.contains('ng-dirty');
+
+        expect(isPristine).toBe(false);
+        expect(isDirty).toBe(true);
+    });
+
+    it('{valid: true} sets ng-valid and unsets ng-invalid', () => {
+        testComponent.componentInstance.summary$.next({
+            ...getFormControlSummary(initFormControl([''])),
+            valid: true,
+        });
+
+        // tslint:disable-next-line: no-string-literal
+        const isValid = directive['ref'].nativeElement.classList.contains('ng-valid');
+        // tslint:disable-next-line: no-string-literal
+        const isInvalid = directive['ref'].nativeElement.classList.contains('ng-invalid');
+
+        expect(isValid).toBe(true);
+        expect(isInvalid).toBe(false);
+    });
+
+    it('{valid: false} sets ng-invalid and unsets ng-valid', () => {
+        testComponent.componentInstance.summary$.next({
+            ...getFormControlSummary(initFormControl([''])),
+            valid: false,
+        });
+
+        // tslint:disable-next-line: no-string-literal
+        const isValid = directive['ref'].nativeElement.classList.contains('ng-valid');
+        // tslint:disable-next-line: no-string-literal
+        const isInvalid = directive['ref'].nativeElement.classList.contains('ng-invalid');
+
+        expect(isValid).toBe(false);
+        expect(isInvalid).toBe(true);
+    });
+
+    it('{untouched: true} sets ng-untouched and unsets ng-touched', () => {
+        testComponent.componentInstance.summary$.next({
+            ...getFormControlSummary(initFormControl([''])),
+            untouched: true,
+        });
+
+        // tslint:disable-next-line: no-string-literal
+        const isUntouched = directive['ref'].nativeElement.classList.contains('ng-untouched');
+        // tslint:disable-next-line: no-string-literal
+        const isTouched = directive['ref'].nativeElement.classList.contains('ng-touched');
+
+        expect(isUntouched).toBe(true);
+        expect(isTouched).toBe(false);
+    });
+
+    it('{untouched: false} sets ng-touched and unsets ng-untouched', () => {
+        testComponent.componentInstance.summary$.next({
+            ...getFormControlSummary(initFormControl([''])),
+            untouched: false,
+        });
+
+        // tslint:disable-next-line: no-string-literal
+        const isUntouched = directive['ref'].nativeElement.classList.contains('ng-untouched');
+        // tslint:disable-next-line: no-string-literal
+        const isTouched = directive['ref'].nativeElement.classList.contains('ng-touched');
+
+        expect(isUntouched).toBe(false);
+        expect(isTouched).toBe(true);
+    });
+});
