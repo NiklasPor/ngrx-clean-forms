4291f02c6d849b75202f62f79db3d9cdc02461fc
NiklasPor <niklaspor@gmail.com>
2020-02-23 15:51:35 +0100

    add tsdoc

--- projects/ngrx-clean-forms/src/lib/reset.ts
+++ projects/ngrx-clean-forms/src/lib/reset.ts
@@ -8,6 +8,15 @@ import {
 import { initFormControl, initFormGroup, initFormArray } from './init';
 import { mapFormGroupControlStates } from './utils';
 
+/**
+ * Resets a control back to the default values.
+ * The following values will stay the same after the reset:
+ * - `initialValue`
+ * - `validators`
+ *
+ * @param control The `FormControlState` which should be used to create the reset.
+ * @param initialValue Optional parameter for passing a new initial value.
+ */
 export function resetFormControl<T>(
     control: FormControlState<T>,
     initialValue = control.initialValue
@@ -15,6 +24,14 @@ export function resetFormControl<T>(
     return initFormControl([initialValue, control.validators]);
 }
 
+/**
+ * Resets a group back to its default values.
+ * Internally calls `resetFormControl` for every control.
+ *
+ * @see `resetFormControl`
+ *
+ * @param group The `FormGroupState` which should be used to create the reset.
+ */
 export function resetFormGroup<TControls extends FormControls>(
     group: FormGroupState<TControls>
 ): FormGroupState<TControls> {
@@ -26,6 +43,14 @@ export function resetFormGroup<TControls extends FormControls>(
     return initFormGroup(initTuple);
 }
 
+/**
+ * Resets an array back to its default values.
+ * Internally calls `resetFormControl` for every control.
+ *
+ * @see `resetFormControl`
+ *
+ * @param array The `FormArrayState` which should be used to create the reset.
+ */
 export function resetFormArray<T>(array: FormArrayState<T>): FormArrayState<T> {
     const initTuple = array.controls.map(
         ({ initialValue, validators }): FormControlInitTuple<any> => [initialValue, validators]
